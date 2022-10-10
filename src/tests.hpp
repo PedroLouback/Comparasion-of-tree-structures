0fa8d93260fc1ec6b3f29cdec5b66b1435b6c3a4
Pedro <pcampos952@gmail.com>
2022-10-10 08:28:24 -0300

    Ordenação e pesquisa binária com vetores funcional

--- src/tests.hpp
+++ src/tests.hpp
@@ -9,5 +9,8 @@ void TestsBinaryTree(vector<float> query_numbers);
 void TestsAVLTree(vector<float> query_numbers);
 void TestsRBTree(vector<float> query_numbers);
 void TestsWithVectors(vector<float> query_numbers);
+vector<float> heapify(vector<float> vector_entries, int n, int i);
+vector<float> heapSort(vector<float> vector_entries, int n);
+int binarySearch(vector<float> vector_entries, int l, int r, float number);
 
 #endif
